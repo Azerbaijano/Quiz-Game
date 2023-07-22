d91b758131afe606e7e6555c2b8f76d5c51becd4
Azerbaijano <136532697+Azerbaijano@users.noreply.github.com>
2023-07-23 11:26:29 +1200

    Update QuestionsCode.py

--- QuestionsCode.py
+++ QuestionsCode.py
@@ -1,12 +1,12 @@
 import openpyxl as open
+import os
 
 # Make sure excel file is in same folder.
-file_path = r'C:\Users\ericl\OneDrive - Onslow College\2022\11 DIT\School\Questions.xlsx'
+current_directory = os.path.dirname(os.path.realpath(__file__))
+file_path = os.path.join(current_directory, "Questions.xlsx")
 workbook = open.load_workbook(file_path)
 worksheet = workbook['Sheet1']
-list = ["A", "B", "C", "D", "E"]
 
-questionno = 1
 
 def askquestion(questionno):
     cell = worksheet["A{}".format(questionno + 1)]
@@ -31,25 +31,18 @@ def askquestion(questionno):
         if answer in c_answerslist:
             print("You got the correct answer. You now have ${}.".format(level))
             valid = True
+            return True
         elif answer in w_answerslist:
             print("You got the wrong answer. You won nothing.")
             print("The correct answer was {}.".format(c_answerslist[1]))    
-            return level
+            return False
         else:
             print("Not a valid answer.")
 
-for i in range(0,11):
-    askquestion(questionno)
+questionno = 1
+while askquestion(questionno):
     questionno += 1
-
-#amountwon = askquestion(questionno)
-#for i in range(0,11):
-    #if amountwon == None:
-        #questionno += 1
-        #askquestion(questionno)
-        #print("You won ${}.".format(amountwon))
-        #amountwon = None
-    #else: 
-        #break
-
+cell = worksheet["A{}".format(questionno + 1)]
+amountwon = cell.value
+print("You won ${}.".format(amountwon))
 
