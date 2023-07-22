b64b3de23eefb866068755a4564a10ded4224582
Azerbaijano <136532697+Azerbaijano@users.noreply.github.com>
2023-07-22 18:17:41 +1200

    Update QuestionsCode.py

--- QuestionsCode.py
+++ QuestionsCode.py
@@ -8,40 +8,48 @@ list = ["A", "B", "C", "D", "E"]
 
 questionno = 1
 
-cell = worksheet["{}{}".format(list[0], questionno + 1)]
-level = cell.value
-cell = worksheet["{}{}".format(list[1], questionno + 1)]
-question = cell.value
-cell = worksheet["{}{}".format(list[2], questionno + 1)]
-options = cell.value
-optionlist = options.split(',')
-cell = worksheet["{}{}".format(list[3], questionno + 1)]
-c_answers = cell.value
-c_answerslist = c_answers.split(',')
-cell = worksheet["{}{}".format(list[4], questionno + 1)]
-w_answers = cell.value
-w_answerslist = w_answers.split(',')
-
-
-
-def askquestion(level, question, options, c_answers, w_answers):
+def askquestion(questionno):
+    cell = worksheet["A{}".format(questionno + 1)]
+    level = cell.value
+    cell = worksheet["B{}".format(questionno + 1)]
+    question = cell.value
+    cell = worksheet["C{}".format(questionno + 1)]
+    options = cell.value
+    optionlist = options.split(',')
+    cell = worksheet["D{}".format(questionno + 1)]
+    c_answers = cell.value
+    c_answerslist = c_answers.split(',')
+    cell = worksheet["E{}".format(questionno + 1)]
+    w_answers = cell.value
+    w_answerslist = w_answers.split(',')
     print("For ${}: {}".format(level, question))
-    for i in options:
+    for i in optionlist:
         print(i)
     valid = False
     while valid is False:
         answer = input("Enter your answer:").upper().strip()
-        if answer in c_answers:
+        if answer in c_answerslist:
             print("You got the correct answer. You now have ${}.".format(level))
             valid = True
-        elif answer in w_answers:
+        elif answer in w_answerslist:
             print("You got the wrong answer. You won nothing.")
-            print("The correct answer was {}.".format(c_answers[1]))
-            valid = True
+            print("The correct answer was {}.".format(c_answerslist[1]))    
             return level
         else:
             print("Not a valid answer.")
-winning = True
-while winning is True:
-    winning = askquestion(level, question, optionlist, c_answerslist, w_answerslist)
-print("You won ${}.".format(winning))
+
+for i in range(0,11):
+    askquestion(questionno)
+    questionno += 1
+
+#amountwon = askquestion(questionno)
+#for i in range(0,11):
+    #if amountwon == None:
+        #questionno += 1
+        #askquestion(questionno)
+        #print("You won ${}.".format(amountwon))
+        #amountwon = None
+    #else: 
+        #break
+
+
